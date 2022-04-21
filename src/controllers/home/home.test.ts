6c62472d44f9fd90c86ae8f37f8869cb779d9b4a
leobtt <leonarbat8@gmail.com>
2022-04-21 14:58:33 -0300

    sort out the test

--- src/controllers/home/home.test.ts
+++ src/controllers/home/home.test.ts
@@ -1,28 +1,70 @@
 import app from '../../server'
 import request from 'supertest'
+import sinon from 'sinon'
+
+import  {home, calc} from './'
 
 describe('Home routes', () => {
-  it('calcular o valor se passado', async () => {
-    const responseQuery = await request(app).get('/calc?n1=5&n2=3')
+    
+  
 
-    expect(responseQuery.body).toMatchObject({
-      valorDaSoma: 8
+  describe('Calcular', () => {
+    
+    it('calcular o valor se passado', async () => {
+      const res: any = {
+        render: function(){}
+      }
+      const req: any = {
+        query: {
+          n1: 4,
+          n2: 4
+        }
+      }
+  
+      let mock = sinon.mock(res)
+      mock.expects('render').once().withArgs('calc', {
+        valorDaSoma : 8
+      })
+      calc(req,res)
     })
+  
+    it('Nenhum número fornecido', async () => {
+      const req: any = {
+        query: {}
+      }
+      const res: any = {
+        render: function(){}
+      }
 
-    const response = await request(app).get('/calc')
+      let mock = sinon.mock(res)
+      mock.expects('render').once().withArgs('calc', { 
+        valorDaSoma: 'nenhum número fornecido'
+      })
+      calc(req,res)
 
-    expect(response.body).toMatchObject({
-      valorDaSoma: 'nenhum número fornecido'
-    })
+    }) 
   })
 
   it('home message', async () => {
-    const response = await request(app).get('/').expect(200)
+    const req: any = {}
+    const res: any = {
+      render: function(){}
+    }
 
-    expect(response.body).toMatchObject({
-      ok : "Fullstack Master"
+    let mock = sinon.mock(res)
+    mock.expects('render').once().withArgs('home', { 
+      time: 10,
+      list: ['Leonardo','Rodrigo','Jessica'],
+      animals: [
+        { name: 'cacy' },
+        { name: 'tihuana' },
+        { name: 'Qqi' }
+      ]
     })
+
+    home(req, res)
   })
+
   it('Page message', async () => {
     const response = await request(app).get('/pagina').expect(200)
 
@@ -32,14 +74,12 @@ describe('Home routes', () => {
   describe('verificando número', () => {
     it('verificando número par', async () => {
       const response = await request(app).get('/par/2')
-      console.log(response.text)
       expect(response.status).toEqual(200)
       expect(response.text).toEqual('Número é par')
     })
     it('verificando número impar', async () => {
       const response = await request(app).get('/par/5')
   
-      console.log(response.text)
       expect(response.status).toEqual(200)
       expect(response.text).toEqual('Número é impar')
     })
